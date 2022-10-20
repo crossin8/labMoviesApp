d4469a863593c18d746825102573cfcec846d60e
crossin8 <20101158@mail.wit.ie>
2022-10-20 14:22:27 +0200

    Add to favourites logic completed; Dummy favourites page.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
-import sample from './stories/sampleData';
-import MovieDetailsPage from './pages/movieDetailsPage';
-const movies = [sample, sample, sample, sample, sample, sample, sample];
+import MoviePage from "./pages/movieDetailsPage";
+import favouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import {Link} from 'react-router-dom'
 
 const App = () => {
-    return (
-        <MovieDetailsPage movie={sample} images={images} />
-        );
-  };
+  return (
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies/favourites">Favourites</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path="/movies/favourites" element={<favouriteMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-const images = [
-    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-  ]
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App /> );
