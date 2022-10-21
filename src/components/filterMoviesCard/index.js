18aa8956548a098aeb5e855116d497fe14166c72
crossin8 <20101158@mail.wit.ie>
2022-10-21 19:22:15 +0200

    Added filtering of movie list.

--- src/components/filterMoviesCard/index.js
+++ src/components/filterMoviesCard/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -10,22 +9,39 @@ import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
-
-const formControl = 
-  {
-    margin: 1,
-    minWidth: 220,
-    backgroundColor: "rgb(255, 255, 255)"
-  };
+import { getGenres } from "../../api/tmdb-api";
+import React, {useState, useEffect}  from "react";
 
 
 export default function FilterMoviesCard(props) {
+  const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
-  const genres = [
-    {id: 1, name: "Animation"},
-    {id: 2, name: "Comedy"},
-    {id: 3, name: "Thriller"}
-  ]
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        process.env.REACT_APP_TMDB_KEY
+    )
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json.genres) 
+        return json.genres
+      })
+      .then(apiGenres => {
+        setGenres([genres[0], ...apiGenres]);
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleChange = (e, type, value) => {
+    e.preventDefault()
+    // Completed later
+  };
+  const handleTextChange = e => {
+    handleChange(e, "name", e.target.value)
+  }
+  const handleGenreChange = e => {
+    handleChange(e, "genre", e.target.value)
+  };
 
   return (
     <Card 
@@ -40,18 +56,23 @@ export default function FilterMoviesCard(props) {
           Filter the movies.
         </Typography>
         <TextField
-          sx={formControl}
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <FormControl sx={formControl}>
+      sx={FormControl}
+      id="filled-search"
+      label="Search field"
+      type="search"
+      variant="filled"
+      value={props.titleFilter}
+      onChange={handleTextChange}
+      />
+        <FormControl sx={FormControl}>
           <InputLabel id="genre-label">Genre</InputLabel>
           <Select
-            labelId="genre-label"
-            id="genre-select"
-          >
+    labelId="genre-label"
+    id="genre-select"
+    defaultValue=""
+    value={props.genreFilter}
+    onChange={handleGenreChange}
+  >
             {genres.map((genre) => {
               return (
                 <MenuItem key={genre.id} value={genre.id}>
@@ -75,10 +96,6 @@ export default function FilterMoviesCard(props) {
         </Typography>
       </CardContent>
     </Card>
-    
+  
   );
-  const handleChange = (e, type, value) => {
-      e.preventDefault()
-      props.onUserInput(type, value)   // NEW
-    }
-}
+          }
